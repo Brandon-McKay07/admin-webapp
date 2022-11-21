7dce1fe08adb6d090e068778bb4eac5976774787
BrandonMcKay <brandonmckay07@gmail.com>
2022-11-20 20:02:47 -0800

    new create product category model box

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { ProductsService } from 'src/app/services/products.service';
 import { CategoriesService } from 'src/app/services/categories.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
@@ -14,9 +15,18 @@ export class ProductsComponent implements OnInit {
  
  public prodObservable: Observable<any[]> = new Observable();
  public categoryObservable: Observable<any[]> = new Observable();
+
+ productForm: FormGroup = new FormGroup({});
+ productModel: Products | undefined;
+ selectedImageIdx: number = 0;
+  thumbnailImageIdx: number = 0;
+  tempImageFiles: any[] = [];
+  updation: boolean = false;
+  loader: boolean = false;
  
  
-  constructor(private productService: ProductsService, private modalService: NgbModal, private categoriesService:CategoriesService){}
+  constructor(private productService: ProductsService, private modalService: NgbModal, 
+    private categoriesService:CategoriesService, private fb:FormBuilder){}
 
 
   ngOnInit(): void {
@@ -27,7 +37,131 @@ export class ProductsComponent implements OnInit {
     
  
   }
+  openModal(modal: any, prd: Products | null = null) {
+    this.tempImageFiles = []; 
+    this.initialiseModal(prd);
+    this.modalService.open(modal, { size: "xl" });
+  }
 
 
-} 
+  initialiseModal(productObj: Products | null) {
+    if (productObj == null) {
+      this.updation = false;
+      this.productForm = this.fb.group({
+        productId: [],
+        productTitle: [null],
+        price: [null],
+        images: this.fb.array([]),
+        thumbnailImage: [null],
+        productDescription: [null],
+        productCategory: [null],
+        active: [true],
+        addedOn: [],
+        rating: [0]
+      });
+    } else {
+      this.updation = true;
+      this.productForm = this.fb.group({
+        productId: [productObj.productId],
+        productTitle: [productObj.productTitle],
+        price: [productObj.price],
+        images: [productObj.images],
+        thumbnailImage: [productObj.thumbnailImage],
+        productDescription: [productObj.productDescription],
+        productCategory: [productObj.productCategory],
+        active: [productObj.active],
+        addedOn: [productObj.addedOn],
+        rating: [productObj.rating]
+      });
+      this.onSelectOption(productObj.productCategory);
+      this.tempImageFiles = productObj.images || [];
+    }
+  }
+
+  compareByCategoryId(category1: Category, category2: Category) {
+    return category1 && category2 && category1.categoryId === category2.categoryId;
+  }
+
+  onSelectOption(category: Category | Event | undefined) {
+    // this.productForm.patchValue({
+    //   category: this.categories.find(x => x.categoryId === category.categoryId)
+    // })
+  }
+
+  // view image model
+  openImageModal(modal: any, imageUrls: string[], thumbnailImageIdx: number) {
+    this.tempImageFiles = [...imageUrls];
+    this.thumbnailImageIdx = thumbnailImageIdx;
+    this.modalService.open(modal, { 
+      size: "xl",
+      scrollable: true 
+    });
+  }
+
+  // open image
+  openImage(url: string) {
+    window.open(url, "_blank")
+  }
+
+  // view product details
+  viewProductDetails(modal: any, productObj: Products) {
+    this.productModel = productObj;
+    this.modalService.open(modal, { size: 'lg' });
+  }
+
+
+  checkImageFileType(event: any) {
+    let fileList: File[] = Object.assign([], event.target.files);
+    fileList.forEach((file: any, idx: number) => {
+      if (
+        file.type == "image/png" ||
+        file.type == "image/jpeg" ||
+        file.type == "image/jpg"
+      ) {
+        this.tempImageFiles.push(file);
+      } else {
+        // this.toast.warning("Only .png/.jpeg/.jpg file format accepted!!", file.name + " will not accepted.");
+      }
+    });
+  }
+
+
+  removeImage(idx: number) {
+    this.tempImageFiles.splice(idx, 1);
+  }
+
+  changeThumbnailImageIdx(idx: number) {
+    this.productForm.patchValue({
+      thumbnailImage: idx
+    })
+  }
+
+
+}
+
+
+export interface Products {
+  productId?: string;
+  productTitle?: string;
+  productCode?: string;
+  productDescription?: string;
+  price?: number;
+  productCategory?: Category;
+  images?: string[];
+  thumbnailImage?: number;
+  active?: boolean;
+  addedOn?: Date;
+  rating?: number;
+}
+
+export interface Category {
+  categoryId?: string;
+  categoryName?: string;
+  categoryDescription?: string;
+  categoryImageUrl?: string;
+  createdOn?: Date;
+  active?: boolean;
+}
+
+
 
